1a9b15d455fb01fb42220c1aa49c77abbc399407
Rono <victorrono18@gmail.com>
2023-11-16 14:25:52 +0300

    dependencies

---
+++ server/index.js
@@ -0,0 +1,17 @@
+//imports
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+//initialize 
+
+const app = express()
+
+//
+const PORT = process.env.PORT || 5000;
+
+//connect to mongoDB 
+
+
